7c5381424aa8d80f10c86fe2da5b998feafbff7f
Daniel Ribeiro <djbreves@gmail.com>
2018-01-06 15:44:02 +1100

    Add sell_category_id to product

--- lib/tidyhqrb/product.rb
+++ lib/tidyhqrb/product.rb
@@ -6,11 +6,13 @@ module Tidyhqrb
 
     attribute :id, String
     attribute :name, String
+    attribute :sell_category_id, Integer
 
     def to_json(*opt)
       {
         id: id,
-        name: name
+        name: name,
+        sell_category_id: sell_category_id
       }.to_json(*opt)
     end
   end
