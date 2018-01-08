4a82fc50049e57b47f94d7882ba4e5c2fe4b0060
Daniel Ribeiro <djbreves@gmail.com>
2018-01-08 22:32:23 +1100

    Add sell price

--- lib/tidyhqrb/product.rb
+++ lib/tidyhqrb/product.rb
@@ -7,12 +7,14 @@ module Tidyhqrb
     attribute :id, String
     attribute :name, String
     attribute :sell_category_id, Integer
+    attribute :sell_price, Integer
 
     def to_json(*opt)
       {
         id: id,
         name: name,
         sell_category_id: sell_category_id
+        sell_price: sell_price
       }.to_json(*opt)
     end
   end
