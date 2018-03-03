fd373639d111d316b64231a3508eb92badad9b9b
Daniel Ribeiro <djbreves@gmail.com>
2018-03-03 16:03:17 +1100

    Add address

--- lib/tidyhqrb/contact.rb
+++ lib/tidyhqrb/contact.rb
@@ -9,6 +9,10 @@ module Tidyhqrb
     attribute :last_name, String
     attribute :email_address, String
     attribute :phone_number, String
+    attribute :address1, String
+    attribute :city, String
+    attribute :state, String
+    attribute :postcode, String
     attribute :created_at, DateTime
 
     def to_json(*opt)
@@ -18,6 +22,10 @@ module Tidyhqrb
         last_name: last_name,
         email_address: email_address,
         phone_number: phone_number,
+        address1: address1,
+        city: city,
+        state: state,
+        postcode: postcode,
         created_at: created_at
       }.to_json(*opt)
     end
